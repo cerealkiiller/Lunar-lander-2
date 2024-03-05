6fe6d1292ddc5aae082933968fb07f6191626347
cerealkiiller <adi2.pant@gmail.com>
2024-03-05 22:30:26 +0530

    Added full lander control

--- Assets/code/lander.cs
+++ Assets/code/lander.cs
@@ -7,30 +7,62 @@ public class lander : MonoBehaviour
     private bool press = false;
     public Rigidbody2D myrigidbody;
     public float jump = 2;
+    private bool left = false;
+    private bool right = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
-            {
+        {
 
-                press = true;
-            }
-            else if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
-            {
-                press = false;
-            }
+            press = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
+        {
+            press = false;
+        }
 
 
-            if (press)
-            {
-                myrigidbody.velocity += Vector2.up * jump;
+        if (press)
+        {
+            myrigidbody.AddForce(transform.up*jump);
 
-            }
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            left = true;
+
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            left = false;
+
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            right = true;
+        }
+
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            right = false;
+        }
+
+        if (right)
+        {
+            transform.Rotate(0.0f, 0.0f, -0.5f, Space.Self);
+        }
+        else if (left)
+        {
+            transform.Rotate(0.0f, 0.0f, 0.5f, Space.Self);
+        }
+
+
     }
 }
